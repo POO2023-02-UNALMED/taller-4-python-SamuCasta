c12fdb7f9cedee49632422a35be06b808096e474
Samuel Castaño Mira <scastanomi@unal.edu.co>
2023-09-02 14:47:39 -0500

    Cambio atributos
    Cambio constructor

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,7 +3,7 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
@@ -14,9 +14,9 @@ class Grupo:
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        if self.listadoAlumnos is None:
-            self.listadoAlumnos = []
+    def agregarAlumno(self, alumno, lista=None):
+        if lista is None:
+            lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
